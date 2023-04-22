8933de288c7da71aab6a721d8a07730b920687f2
klin2020 <karen.lin@yale.edu>
2023-04-22 19:15:27 -0400

    reformatted start_scene and instruction_scene to have rectangle buttons to move to new scenes

--- src/instruction_scene.js
+++ src/instruction_scene.js
@@ -23,26 +23,58 @@ var instruction_scene = {
     pointer = this.add.circle(0, 0, 10, '0xff0000');
     leftPointer = this.add.circle(0, 0, 10, '0x00ff00');
 
+    // startButton = this.add.rectangle(width/2 + 300, height - 75, 200, 100, "0xffffff");
+    instructionExitButton = this.add.rectangle(75, 50, 100, 50, "0xffffff");
+    this.make.text({
+      x: 75,
+      y: 50,
+      text: "Exit",
+      origin: {x: 0.5, y: 0.5},
+      style: {
+          font: 'bold 30px Arial',
+          fill: 'black',
+          wordWrap: {width: 600},
+          align: "left"
+      }
+    }); 
+  
+    instructionButton = this.add.rectangle(width/2 + 300, height - 75, 200, 100, "0xffffff");
+    this.make.text({
+      x: width/2 + 300,
+      y: height-75,
+      text: "Start Game",
+      origin: {x: 0.5, y: 0.5},
+      style: {
+          font: 'bold 30px Arial',
+          fill: 'black',
+          wordWrap: {width: 600},
+          align: "left"
+      }
+    }); 
   }
   
-  function create_instruction () {
+var instructionClickTime = null;
+var instructionVerifyTime = 3;
+
+function create_instruction () {
     timedEvent = this.time.addEvent({ delay: 9999999, callback: this.onClockEvent, callbackScope: this, repeat: 1 });
 
     this.make.text({
-        x: width/2 - 260,
-        y: 50,
+        x: width/2 - 270,
+        y: 110,
         text: "This is a one player game.",
         origin: {x: 0.5, y: 0.5},
         style: {
             font: 'bold 30px Arial',
             fill: 'white',
-            // wordWrap: {width: 600},
+            wordWrap: {width: 600},
             align: "left"
         }
     });
+
     this.make.text({
-      x: width/2 - 150,
-      y: 135,
+      x: width/2 - 165,
+      y: 190,
       text: "Hit as many circles as you can with your hands in 90 seconds to add to your residential college score!",
       origin: {x: 0.5, y: 0.5},
       style: {
@@ -53,23 +85,23 @@ var instruction_scene = {
       }
   });
 
-    this.make.text({
-      x: width/2 - 140,
-      y: 250,
-      text: "Hitting circles will gain you points. Hitting squares will lose you points.",
-      origin: {x: 0.5, y: 0.5},
-      style: {
-          font: 'bold 30px Arial',
-          fill: 'white',
-          wordWrap: {width: 650},
-          align: "left"
-      }
+  this.make.text({
+    x: width/2 - 155,
+    y: 300,
+    text: "Hitting circles will gain you points. Hitting squares will lose you points.",
+    origin: {x: 0.5, y: 0.5},
+    style: {
+        font: 'bold 30px Arial',
+        fill: 'white',
+        wordWrap: {width: 650},
+        align: "left"
+    }
   });
 
   this.make.text({
-    x: width/2 - 180,
-    y: 350,
-    text: "Point at the rectangle in the top left corner to exit the game at any time",
+    x: width/2 - 200,
+    y: 390,
+    text: "Point at the rectangle in the top left corner to exit the game at any time.",
     origin: {x: 0.5, y: 0.5},
     style: {
         font: 'bold 30px Arial',
@@ -79,24 +111,77 @@ var instruction_scene = {
     }
   }); 
 
-  }
+  this.make.text({
+    x: width/2 - 175,
+    y: 480,
+    text: "Point at the rectangle in on the right to start the game!",
+    origin: {x: 0.5, y: 0.5},
+    style: {
+        font: 'bold 30px Arial',
+        fill: 'white',
+        wordWrap: {width: 600},
+        align: "left"
+    }
+  }); 
+
+}
   
 
   
   function update_instruction () {
+    let elapsedTime = timedEvent.getElapsedSeconds();
 
   
-    if (hand_x) {
+  if (hand_x || leftHand_x) {
       //update hand points
       updatePointer(pointer, width - hand_x, hand_y);
       updatePointer(leftPointer, width - leftHand_x, leftHand_y);
   
 
     }
+
+  //hover over Start Game for 3 seconds
+  if (Phaser.Geom.Intersects.CircleToRectangle(instructionButton, pointer) 
+  || Phaser.Geom.Intersects.CircleToRectangle(instructionButton, leftPointer)) {
+      if (startClickTime == null) {
+        startClickTime = elapsedTime;
+      } 
+      else {
+        console.log(elapsedTime - startClickTime);
+        if (elapsedTime - startClickTime > startVerifyTime) {
+          this.scene.start('game_scene');
+        }
+      }
+  } 
+  else {
+    startClickTime = null;
+  }
+
+  //hover over Exit for 3 seconds
+  if (Phaser.Geom.Intersects.CircleToRectangle(instructionExitButton, pointer) 
+  || Phaser.Geom.Intersects.CircleToRectangle(instructionExitButton, leftPointer)) {
+    if (startClickTime == null) {
+        startClickTime = elapsedTime;
+    } 
+    else {
+        console.log(elapsedTime - startClickTime);
+        if (elapsedTime - startClickTime > startVerifyTime) {
+          this.scene.start('start_scene');
+        }
+    }
+  } 
+  else {
+    startClickTime = null;
+  }
   }
   
-  function updatePointer(p, x, y) {
-    p.x = x;
-    p.y = y;
+  function updatePointers(p) {
+    if (hand_x || leftHand_x) {
+      // console.log('updating hand');
+      // console.log(hand_x);
+      // console.log(hand_y);
+      updatePointer(pointer, width - hand_x, hand_y);
+      updatePointer(leftPointer, width - leftHand_x, leftHand_y);
+    }
   }
   
--- src/start_scene.js
+++ src/start_scene.js
@@ -4,13 +4,33 @@ var start_scene = {
   update: update_start,
 };
 
-timeLimitStart = 1
-function create_start () {
+timeLimitStart = 0;
+
+function preload(){
+  this.load.setBaseURL('http://labs.phaser.io');
   width = this.sys.game.canvas.width;
   height = this.sys.game.canvas.height;
+  pointer = this.add.circle(0, 0, 10, '0xff0000');
+  leftPointer = this.add.circle(0, 0, 10, '0x00ff00');
 
-  // timeText = this.add.text(150, 20, "",{ fontSize: 24 }).setOrigin(0.5,0.5);
-  // scoreText = this.add.text(700, 20, "",{ fontSize: 24 }).setOrigin(0.5,0.5);
+  startButton = this.add.rectangle(width/2 + 300, height - 75, 200, 100, "0xffffff");
+  this.make.text({
+    x: width/2 + 300,
+    y: height - 75,
+    text: 'Start',
+    origin: { x: 0.5, y: 0.5},
+    style: {
+      font: 'bold 30px Arial',
+      fill: 'black',
+      wordWrap: {width: 600},
+      align: "left"
+    }
+  });
+
+}
+function create_start () {
+
+
   timedEvent = this.time.addEvent({ delay: 9999999, callback: this.onClockEvent, callbackScope: this, repeat: 1 });
   score = 0;
   this.make.text({
@@ -27,61 +47,101 @@ function create_start () {
   });
 
   this.make.text({
-    x: width/2 - 275,
+    x: width/2 - 150,
     y: height - 50,
-    text: 'Raise your hand to the ellipse to begin',
+    text: 'Raise your hand to the rectangle to begin',
     origin: { x: 0.5, y: 0.5},
     style: {
-      font: 'bold 20px Arial',
+      font: 'bold 30px Arial',
       fill: 'white',
       wordWrap: {width: 600},
       align: "left"
     }
   });
   
-  this.add.ellipse(width/2 + 100, height - 75, 200, 100, "0xffffff");
-
-
- 
-  //this.add.circle(width/2, height - 50, 100, "#ffffff");
+  
 
 
 }
 
-var oldTime = 0;
+var prevTime = 0;
+var arrIndex = 0;
+var shapeArr = new Array(10);
+var startClickTime = null;
+var startVerifyTime = 3;
 
 function update_start () {
-  // console.log('updating');
+  updatePointers();
 
   // update timer
   let elapsedTime = timedEvent.getElapsedSeconds();
-  let timeLeft = timeLimitStart - elapsedTime;
-  console.log(timeLimitStart);
-
-  this.add.ellipse(width/2 + 100, height - 75, 200, 100, "0xffffff");
-
-
-  //display time and score
-  // timeText.setText('Time Remaining: ' + Math.floor(timeLeft).toString());
-  // scoreText.setText('Score: ' + score.toString());
-
-
+  
+  var graphics = this.add.graphics();
     //create new circle every second until time runs out
-    if (parseInt(oldTime) != parseInt(timeLeft)){
+    if (parseInt(prevTime) != parseInt(elapsedTime)){
+      console.log(prevTime);
+
+      //randomize position and size of circle
       let x = Phaser.Math.Between(0, width);
       let y = Phaser.Math.Between(height-350, height - 175);
       let size = Phaser.Math.Between(10, 50);
+
+      //randomize color
       let color = new Phaser.Display.Color();
       color = color.random();
-      let shape = this.add.circle(x, y, size, color.color);
-      shape = this.add.group();
+      let color32 = Phaser.Display.Color.GetColor32(color["r"], color["g"], color["b"], color["a"]);
 
-      // shape = this.add.circle(x, y, size, '0x00bfff');
-      oldTime = timeLeft;
+      //create circle 
+      let shape = graphics.fillStyle(color32);
+      shape = graphics.fillCircle(x, y, size);
+
+      //add circle to array
+      shapeArr[arrIndex] = shape;
+      arrIndex++;
+      
+      
+      prevTime =  elapsedTime; //update second
     }
 
-  if (timeLeft <= 0) {
-    //reset circles, loop
-    this.scene.start('instruction_scene');
+    //reset circles every 10 seconds
+    if (parseInt(elapsedTime) != 0 && parseInt(elapsedTime) % 10 == 0){
+      //destroy all objects in the array
+      // console.log("10 seconds passed");
+      for(var i = 0; i < 10; i++){
+        shapeArr[i].destroy();
+      }
+      arrIndex = 0;
+    }
+
+    //hover over Start for 3 seconds
+    if (Phaser.Geom.Intersects.CircleToRectangle(startButton, pointer) 
+    || Phaser.Geom.Intersects.CircleToRectangle(startButton, leftPointer)) {
+      if (startClickTime == null) {
+        startClickTime = elapsedTime;
+      } else {
+        console.log(elapsedTime - startClickTime);
+        if (elapsedTime - startClickTime > startVerifyTime) {
+          this.scene.start('instruction_scene');
+        }
+      }
+    } 
+    else {
+      startClickTime = null;
+    }
+
+    // if(timeLimitStart == 0){
+    //   this.scene.start('instruction_scene');
+    // }
+
+}
+
+function updatePointers(p) {
+  if (hand_x || leftHand_x) {
+    // console.log('updating hand');
+    // console.log(hand_x);
+    // console.log(hand_y);
+    updatePointer(pointer, width - hand_x, hand_y);
+    updatePointer(leftPointer, width - leftHand_x, leftHand_y);
   }
 }
+
